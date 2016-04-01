e475f5ed936e1181be88ed16384ab563a7a2d4dd
ForkEyes <13558798397@163.com>
2016-04-01 11:30:28 +0800

    updata

---
+++ two/2-7.py
@@ -0,0 +1,24 @@
+# -*- coding:utf-8 -*-
+
+'''
+    循环和字串
+    从用户那里接受一个字符串输入，然后逐字符显示该字符串。先用while循
+    环实现，然后再用for循环实现。
+'''
+
+# 字符串有其特有的索引规则:第一个字符的索引是0,最后一个字符的索引是-1。
+# while
+string = raw_input("please enter a string!\n")
+i = 0
+x = len(string)
+while True:
+    if i<x:
+        print string[i]
+        i+= 1
+    else:
+        break
+
+# for
+string = raw_input('please enter a string!\n')
+for x in string:
+    print x
